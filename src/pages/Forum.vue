ec2081057e948622fe30436cffd8bf00e199003a
Tri Sutrisno <mas.trisutrisno@gmail.com>
2022-04-24 22:54:02 +0700

    add loading and progress

--- src/pages/Forum.vue
+++ src/pages/Forum.vue
@@ -1,21 +1,23 @@
 <template>
-  <div v-if="forum" class="col-full push-top">
-    <div class="forum-header">
-      <div class="forum-details">
-        <h1>{{ forum.name }}</h1>
-        <p class="text-lead">{{ forum.description }}</p>
+  <div v-if="asyncDataStatus_ready" class="col-full">
+    <div v-if="forum" class="col-full push-top">
+      <div class="forum-header">
+        <div class="forum-details">
+          <h1>{{ forum.name }}</h1>
+          <p class="text-lead">{{ forum.description }}</p>
+        </div>
+        <router-link
+          :to="{ name: 'ThreadCreate', params: { forumId: forum.id } }"
+          class="btn-green btn-small"
+        >
+          Start a thread
+        </router-link>
       </div>
-      <router-link
-        :to="{ name: 'ThreadCreate', params: { forumId: forum.id } }"
-        class="btn-green btn-small"
-      >
-        Start a thread
-      </router-link>
     </div>
-  </div>
 
-  <div v-if="threadLoaded" class="col-full push-top">
-    <ThreadList :threads="threads" />
+    <div v-if="threadLoaded" class="col-full push-top">
+      <ThreadList :threads="threads" />
+    </div>
   </div>
 </template>
 
@@ -23,8 +25,10 @@
 import ThreadList from "@/components/ThreadList";
 import { findById } from "@/helpers";
 import { mapActions } from "vuex";
+import asyncDataStatus from "@/mixins/asyncDataStatus";
 export default {
   components: { ThreadList },
+  mixins: [asyncDataStatus],
   props: {
     id: {
       required: true,
@@ -53,7 +57,8 @@ export default {
   async created() {
     const forum = await this.fetchForum({ id: this.id });
     const threads = await this.fetchThreads({ ids: forum.threads });
-    this.fetchUsers({ ids: threads.map((thread) => thread.userId) });
+    await this.fetchUsers({ ids: threads.map((thread) => thread.userId) });
+    this.asyncDataStatus_fetched();
     this.threadLoaded = true;
   },
 };
