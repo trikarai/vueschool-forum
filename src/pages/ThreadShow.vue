47b4cc350aa1361e5692a6cc417d97a4926e6031
Tri Sutrisno <mas.trisutrisno@gmail.com>
2022-04-25 12:31:12 +0700

    allowing only auth users to create and edit posts

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,6 +3,7 @@
     <h1>
       {{ thread.title }}
       <router-link
+        v-if="thread.userId === authUser?.id"
         :to="{ name: 'ThreadEdit', id: this.id }"
         class="btn-green btn-small"
         tag="button"
@@ -11,73 +12,75 @@
       </router-link>
     </h1>
     <p>
-      By <a href="#" class="link-unstyled">{{ thread.author?.name }}</a
-      >, <AppDate :timestamp="thread.publishedAt" />.
+      By <a href="#" class="link-unstyled">{{thread.author?.name}}</a>, <AppDate :timestamp="thread.publishedAt" />.
       <span
-        style="float: right; margin-top: 2px"
+        style="float:right; margin-top: 2px;"
         class="hide-mobile text-faded text-small"
-        >{{ thread.repliesCount }} replies by
-        {{ thread.contributorsCount }} contributors</span
+        >{{thread.repliesCount}} replies by {{thread.contributorsCount}} contributors</span
       >
     </p>
 
     <post-list :posts="threadPosts" />
 
-    <post-editor @save="addPost" />
+    <post-editor v-if="authUser" @save="addPost" />
+    <div v-else class="text-center" style="margin-bottom: 50px;">
+      <router-link :to="{name: 'SignIn', query:{redirectTo: $route.path}}">Sign In</router-link> or <router-link :to="{name: 'Register',  query:{redirectTo: $route.path}}">Register</router-link> to reply.
+    </div>
   </div>
 </template>
 
 <script>
-import PostList from "@/components/PostList";
-import PostEditor from "@/components/PostEditor";
-import { mapActions } from "vuex";
-import asyncDataStatus from "@/mixins/asyncDataStatus";
+import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
+import { mapActions, mapGetters } from 'vuex'
+import asyncDataStatus from '@/mixins/asyncDataStatus'
 export default {
-  name: "ThreadShow",
+  name: 'ThreadShow',
   components: {
     PostList,
-    PostEditor,
+    PostEditor
   },
   mixins: [asyncDataStatus],
   props: {
     id: {
       required: true,
-      type: String,
-    },
+      type: String
+    }
   },
   computed: {
-    threads() {
-      return this.$store.state.threads;
+    ...mapGetters(['authUser']),
+    threads () {
+      return this.$store.state.threads
     },
-    posts() {
-      return this.$store.state.posts;
+    posts () {
+      return this.$store.state.posts
     },
-    thread() {
-      return this.$store.getters.thread(this.id);
+    thread () {
+      return this.$store.getters.thread(this.id)
     },
-    threadPosts() {
-      return this.posts.filter((post) => post.threadId === this.id);
-    },
+    threadPosts () {
+      return this.posts.filter(post => post.threadId === this.id)
+    }
   },
   methods: {
-    ...mapActions(["fetchThread", "fetchUsers", "fetchPosts", "createPost"]),
-    addPost(eventData) {
+    ...mapActions(['fetchThread', 'fetchUsers', 'fetchPosts', 'createPost']),
+    addPost (eventData) {
       const post = {
         ...eventData.post,
-        threadId: this.id,
-      };
-      this.createPost(post);
-    },
+        threadId: this.id
+      }
+      this.createPost(post)
+    }
   },
-  async created() {
+  async created () {
     // fetch the thread
-    const thread = await this.fetchThread({ id: this.id });
+    const thread = await this.fetchThread({ id: this.id })
     // fetch the posts
-    const posts = await this.fetchPosts({ ids: thread.posts });
+    const posts = await this.fetchPosts({ ids: thread.posts })
     // fetch the users associated with the posts
-    const users = posts.map((post) => post.userId).concat(thread.userId);
-    await this.fetchUsers({ ids: users });
-    this.asyncDataStatus_fetched();
-  },
-};
+    const users = posts.map(post => post.userId).concat(thread.userId)
+    await this.fetchUsers({ ids: users })
+    this.asyncDataStatus_fetched()
+  }
+}
 </script>
